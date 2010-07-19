4521dd974cbf1c0c0f9511016a3f6c46e30df2ef
Marco Antonio Gonzalez Junior <kayaman@baurets.net>
2010-07-19 20:58:39 -0300

    Bauretsfying and did some real hard effort on RSpec tests.

--- spec/optionsful_spec.rb
+++ spec/optionsful_spec.rb
@@ -1,29 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper'
-# require 'ruby-debug'
 
-describe "Optionsful" do
+
+describe "Baurets::Optionsful," do
+
   include Rack::Test::Methods
-
-  def app
-    @app ||= Rack::Builder.new {
-      use Rack::CommonLogger
-      use Rack::ShowExceptions
-    } 
-  end
-
-  def mock_env(options = {})
-    env = options
-  end
-  
-  def env
-    {"rack.session"=>{:session_id=>"f66c8af1e89f318775e923159e046e71"}, 
-    "SERVER_NAME"=>"localhost", "HTTP_HOST"=>"localhost:3332", "REMOTE_HOST"=>"localhost",
-     "HTTP_USER_AGENT"=>"HTTP%20Client0.9.1 CFNetwork/438.14 Darwin/9.8.0 (i386) (MacBook4%2C1)", 
-     "rack.url_scheme"=>"http", "REQUEST_PATH"=>"/", "SERVER_PROTOCOL"=>"HTTP/1.1", "rack.errors"=>"", 
-     "PATH_INFO"=>"/", "REMOTE_ADDR"=>"127.0.0.1", "SERVER_SOFTWARE"=>"WEBrick/1.3.1 (Ruby/1.8.7/2010-06-23)", 
-     "rack.run_once"=>false, "rack.version"=>[1, 1], "SCRIPT_NAME"=>"", "rack.multithread"=>false, 
-     "HTTP_VERSION"=>"HTTP/1.1", "REQUEST_URI"=>"", "rack.multiprocess"=>false, "SERVER_PORT"=>"3332", "rack.session.options"=>{:domain=>nil, :path=>"/", :key=>"_session_id", :httponly=>true, :expire_after=>nil, :id=>"f66c8af1e89f318775e923159e046e71"}, "REQUEST_METHOD"=>"HEAD", "GATEWAY_INTERFACE"=>"CGI/1.1", "QUERY_STRING"=>"", "HTTP_CONNECTION"=>"close", "rack.input"=> ""}
-  end
 
   before(:all) do
     ActionController::Routing::Routes.draw do |map| 
@@ -31,56 +11,100 @@ describe "Optionsful" do
     end
   end
 
-  after(:all) do
-    ActionController::Routing::Routes.reload!
-  end
 
-  # A Rack application is an Ruby object (not a class) that responds to call. It takes exactly one argument, the
-  # environment and returns an Array of exactly three values: The status, the headers, and the body.
+  describe "as a Rack middleware" do
 
-  describe "as a Rack application," do
-
-    it "is an Ruby object that responds to call" do
-      assert Optionsful.new(app).respond_to?(:call)
+    it "is a Ruby object that responds to call;" do
+      assert Baurets::Optionsful.new(app).respond_to? :call
     end
 
-    it "takes exactly one argument, (the environment) and returns an Array" do
-      assert Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/" })).kind_of?(Array)
+    it "takes exactly one argument, (the environment) and returns an Array;" do
+      response = Baurets::Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts"}))
+      assert response.kind_of?(Array)
     end
 
-    it "returns an Array of exactly three values: The status, the headers, and the body" do
-      response = Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/" }))
+    it "the returned Array must have exactly three values: the status, the headers and the body;" do
+      response = Baurets::Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts"}))
       assert response.size.should == 3
+      assert response[0].kind_of? Fixnum 
+      assert response[1].kind_of? Hash
+      assert response[2].kind_of? String
     end
+    
+    it "must be nice, somewhere on a Rack middleware stack." do
+      response = fake_app.call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts"}))
+      assert response.size.should == 3
+      assert response[0].kind_of? Fixnum
+      assert response[0].should == 204
+      assert response[1].kind_of? Hash
+      assert response[1]["Allow"]
+      # assert response[1]["Allow"].should_not be_nil
+    end
+
   end
 
-  describe " path info recognition " do 
 
-    it "recognize route for single static path part" do
-      assert Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts" })).kind_of?(Array)
+  describe "to collect the allowed HTTP methods" do
+
+    describe "overrides Rails routing recognition " do 
+
+      describe "must understand the default Rails resource routing: " do
+
+        it "the index action displays a list of all posts in response of a GET request;" do
+          response = http_options_request("/posts")
+          assert response.kind_of?(Array)
+          assert response[1]["Allow"]
+        end
+        
+        it "the new action return from a GET request an HTML form for creating a new post;" do
+          response = http_options_request("/posts/new")
+          assert response.kind_of?(Array)
+        end
+        
+        it "the create action uses POST to create a new post instance;" do
+          response = http_options_request("/posts")
+          assert response.kind_of?(Array)
+        end
+        
+        it "the show action display a specific post in response of a GET request;" do
+          response = http_options_request("/posts/1")
+          assert response.kind_of?(Array)
+        end
+        
+        it "the edit action return an HTML form for editing a post in response of a GET request;" do
+          response = http_options_request("/posts/1/edit")
+          assert response.kind_of?(Array)
+        end
+        
+        it "the update action uses PUT to update a specific post;" do
+          response = http_options_request("/posts/1")
+          assert response.kind_of?(Array)
+        end
+        
+        it "the destroy action uses DELETE to delete a specific post." do
+          response = http_options_request("/posts/1")
+          assert response.kind_of?(Array)
+        end 
+        
+      end
+      
     end
-
-    it "recognize route for one static path parts and one dynamic" do
-      assert Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts/1" })).kind_of?(Array)
-    end
-
-    it "recognize route for 2 static path parts and 1 dynamic" do
-      assert Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts/1/comments" })).kind_of?(Array)
-    end
-
-    it "recognize route for 2 static path parts and 2 dynamic" do
-      assert Optionsful.new(app).call(mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => "/posts/1/comments/1" })).kind_of?(Array)
-    end
-
+    
   end
 
   describe " dispatch the call to the application unless HTTP method equals OPTIONS " do
 
     it " method GET example" do
-      response = Optionsful.new(app).call(env)
+      complex_env = mock_env({"REQUEST_METHOD" => "GET", "PATH_INFO" => "/lobster" })
+      response = fake_app.call(complex_env)
+      assert response.kind_of?(Array)
       assert response.size.should == 3
     end
 
+  end
+
+  after(:all) do
+    ActionController::Routing::Routes.reload!
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,67 @@
-require File.join(File.dirname(__FILE__), '..', 'lib','optionsful.rb')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'baurets','optionsful.rb')
 
-require 'rubygems'
-require 'sinatra'
-require 'rack/test'
-require 'rack'
+require 'rubygems' 
+require 'sinatra' 
+require 'rack/test' 
+require 'rack' 
+require 'rack/lobster' 
 require 'spec'
-require 'spec/autorun'
-require 'spec/interop/test'
-require 'action_controller'
+require 'spec/autorun' 
+require 'spec/interop/test' 
+require 'action_controller' 
+require 'ruby-debug'
+# require 'baurets/optionsful'
 
-# set test environment
-set :environment, :test
-set :run, false
-set :raise_errors, true
-set :logging, false
+# set test environment set :environment, :test set :run, false set :raise_errors, true set :logging, false
+
+DEFAULT_ENV = { "rack.version" => Rack::VERSION, "rack.input" => StringIO.new, "rack.errors" => StringIO.new,
+  "rack.multithread" => true, "rack.multiprocess" => true, "rack.run_once" => false, }
+
+  SAMPLE_ENV = {"rack.session"=>{:session_id=>"f66c8af1e89f318775e923159e046e71"}, "SERVER_NAME"=>"localhost",
+  "HTTP_HOST"=>"localhost:3332", "REMOTE_HOST"=>"localhost", "HTTP_USER_AGENT"=>"HTTP%20Client0.9.1
+  CFNetwork/438.14 Darwin/9.8.0 (i386) (MacBook4%2C1)", "rack.url_scheme"=>"http", "REQUEST_PATH"=>"/",
+  "SERVER_PROTOCOL"=>"HTTP/1.1", "rack.errors"=>"", "PATH_INFO"=>"/", "REMOTE_ADDR"=>"127.0.0.1",
+  "SERVER_SOFTWARE"=>"WEBrick/1.3.1 (Ruby/1.8.7/2010-06-23)", "rack.run_once"=>false, "rack.version"=>[1, 1],
+  "SCRIPT_NAME"=>"", "rack.multithread"=>false, "HTTP_VERSION"=>"HTTP/1.1", "REQUEST_URI"=>"",
+  "rack.multiprocess"=>false, "SERVER_PORT"=>"3332", "rack.session.options"=>{:domain=>"nilly", :path=>"/",
+    :key=>"_session_id", :httponly=>true, :expire_after=>300, :id=>"f66c8af1e89f318775e923159e046e71"},
+    "REQUEST_METHOD"=>"HEAD", "GATEWAY_INTERFACE"=>"CGI/1.1", "QUERY_STRING"=>"", "HTTP_CONNECTION"=>"close"}
+
+    FAKE_ENV = SAMPLE_ENV.merge!(DEFAULT_ENV)
+
+    def app
+      app = Rack::Builder.new {
+        use Rack::CommonLogger
+        use Rack::ShowExceptions
+      } 
+    end
+
+
+    def fake_app
+      app = Rack::Builder.new {
+        use Rack::CommonLogger
+        use Rack::ShowExceptions
+        use Baurets::Optionsful
+        map "/lobster" do
+          use Rack::Lint
+          run Rack::Lobster.new
+        end
+      }
+    end
+
+    def mock_env(options = {})
+      FAKE_ENV.merge!(options)
+    end
+
+    def http_options_request(path)
+      complex_env = mock_env({"REQUEST_METHOD" => "OPTIONS", "PATH_INFO" => path })
+      response = Baurets::Optionsful.new(app).call(complex_env)
+      response
+    end
+    
+    def allows?(response, method)
+      response.headers["Allow"].contain?(method)
+    end
+      
+
+
